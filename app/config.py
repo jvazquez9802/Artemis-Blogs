271f9945775a7d191dd5c963d16049d8b66c7937
Juan_Vazquez <jvazquez2098@gmail.com>
2021-01-17 16:52:03 -0600

    Deploy

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config():
    SECRET_KEY  = '52b67e7deddf113d0126e9deb2df9bf9'
-   SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:admin@localhost:5433/Artemis_blogs'
+   SQLALCHEMY_DATABASE_URI =='postgres://topxanpmuaxcln:b9a9c39ec3975ca2f9441c283540639c55d561dda6811255899c2c1f60468e68@ec2-52-204-20-42.compute-1.amazonaws.com:5432/dc1nb2aikf25ip'
    SQLALCHEMY_TRACK_MODIFICATIONS =False
    MAIL_SERVER ='smtp.googlemail.com'
    MAIL_PORT = 587
